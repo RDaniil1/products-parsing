25b27667a8cec3efd9707a56fb6616873e973a67
RDaniil1 <r.daniil1@outlook.com>
2023-08-12 19:55:52 -0400

    main: Fixed webpage logic

--- site_agregator/main/views.py
+++ site_agregator/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from main.sites.avito import get_avito_products
+from main.product.product import Product
+from pprint import pprint
 
 
-def index(request):
-    query = 'switch'
-    products = get_avito_products(query, _filter='cheaper')
+def index(request) -> HttpResponse:
+    if request.method == 'POST' and request.POST['search']:
+        query = request.POST['search']
+        products = get_avito_products(query, _filter='cheaper')
+    else: products = [Product()]
     return render(request, 'main/index.html', {'products' : products})
